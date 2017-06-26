6c6bba7ec1afa39b9bd160dae5d83ea85e013411
ZacDavidM <zacdavidm@gmail.com>
2017-06-26 00:20:01 -0400

    move showtimes json to assets and get movie service basics working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, ModalController } from 'ionic-angular';
+import { MovieService } from '../../app/services/movie.service'
 
+// @IonicPage({
+//   segment: 'home'
+// })
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'home',
+  templateUrl: 'home.html',
+  providers: [MovieService]
 })
 export class HomePage {
+  showtimes: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private movieService: MovieService,
+    public modalCtrl: ModalController,
+  ) {
+    this.getDefaults();
+  }
 
+  getDefaults() {
+    this.showtimes = [];
+  }
+
+  ngOnInit(){
+    this.getShowtimes();
+  }
+
+  getShowtimes(){
+    this.movieService.getShowtimes().subscribe(response => {
+      this.showtimes = response;
+      console.log(this.showtimes);
+    });
   }
 
 }
